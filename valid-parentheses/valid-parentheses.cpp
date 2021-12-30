6df438aabcbec7548c0eb244fc37cbceecb43b1e
mockmoniker <39871035+mockmoniker@users.noreply.github.com>
2021-12-29 23:20:52 -0800

    Time: 5 ms (5.27%), Space: 6.5 MB (9.82%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int type_counts[3] = {0,0,0};  // counters to keep track of open parantheses
+    stack<int> stack;              // stack to keep track of parantheses type
+    
+    void incrementAndPush(int type){
+        type_counts[type]++;
+        stack.push(type);
+    }
+    
+    bool decrementAndPop(int type){
+        if(stack.empty() or type != stack.top() or type_counts[type] == 0){
+            return false;
+        }
+        type_counts[type]--;
+        stack.pop();
+        return true;
+    }
+    
+    bool isValid(string s) {
+        
+        // parse through string
+        for(int i = 0; i < s.length(); ++i){
+            switch(s[i]){
+                case '(':
+                    incrementAndPush(0);
+                    break;
+                case '{':
+                    incrementAndPush(1);
+                    break;
+                case '[':
+                    incrementAndPush(2);
+                    break;
+                case ')':
+                    if(!decrementAndPop(0)){
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(!decrementAndPop(1)){
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(!decrementAndPop(2)){
+                        return false;
+                    }
+                    break;
+            }
+        }
+        if(type_counts[0] + type_counts[1] + type_counts[2] == 0){
+            return true;
+        }
+        return false;
+    }
+};
