9a417085bd6a8562bd6525c570faf8db1377b60e
mockmoniker <39871035+mockmoniker@users.noreply.github.com>
2022-01-04 00:28:22 -0800

    Time: 15 ms (31.30%), Space: 7.4 MB (43.32%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int counts[26] = {0};
+        
+        for(int i = 0; i < s.length(); ++i){
+            counts[s[i]-'a']++;
+        }
+        for(int i = 0; i < t.length(); ++i){
+            counts[t[i]-'a']--;
+        }
+        
+        for(int i = 0; i < 26; ++i){
+            if(counts[i] != 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
