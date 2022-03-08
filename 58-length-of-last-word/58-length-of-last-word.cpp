afa0fd22d0a20b0e111f89b31437277af4019e6f
mockmoniker <39871035+mockmoniker@users.noreply.github.com>
2022-03-08 09:54:41 -0800

    Time: 0 ms (100.00%), Space: 6.5 MB (88.67%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        
+        int i = s.length() - 1;
+        while(i >= 0 and s[i] == ' '){
+            --i;
+        }
+        
+        while(i >= 0 and s[i] != ' '){
+            --i;
+            ++len;
+        }
+        
+        return len;
+    }
+};
